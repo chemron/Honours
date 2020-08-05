de448f5712953a8ea431ba726a345f7713f8fcfc
chemron <camerontasmith@gmail.com>
2020-08-05 18:26:03 +1000

    finished fits retrieval script

--- retrieve_fits.py
+++ retrieve_fits.py
@@ -1,20 +1,43 @@
 import requests  # to get image from the web
 import shutil  # to save it locally
+from datetime import timedelta, datetime
+import os
+
 
 # Set up the image URL and filename
-image_url = "https://miro.medium.com/max/700/1*G0pnrpiSVN01EZCGXiHkYQ.jpeg"
+main_url = "http://jsoc.stanford.edu/data/farside/Phase_Maps/"
 
-# Open the url image, set stream to True, this will return the stream content.
-r = requests.get(image_url, stream=True)
+# files are formated as: PHASE_MAP_yyyy.mm.dd_hh:mm:ss.fits
+start_date = datetime(2014, 9, 7)
+# TODO: make this update to current day
 
-# Check if the image was retrieved successfully
-if r.status_code == 200:
-    r.raw.decode_content = True
+end_date = datetime(2020, 8, 3)
+dt = timedelta(hours=12)
 
-    # Open a local file with wb ( write binary ) permission.
-    with open("test.jpeg", 'wb') as f:
-        shutil.copyfileobj(r.raw, f)
+folder = "./Phase_maps/"
+os.makedirs(folder) if not os.path.exists(folder) else None
 
-    print('Image sucessfully Downloaded')
-else:
-    print('Image Couldn\'t be retreived')
+
+date = start_date
+while date <= end_date:
+    print(f"{date}\r", end="")
+    name = f"PHASE_MAP_{date.year}.{date.month:0>2}." \
+           f"{date.day:0>2}_{date.hour:0>2}:00:00.fits"
+
+    url = main_url + name
+    filename = folder + name
+    # Open the url image and stream content.
+    r = requests.get(url, stream=True)
+
+    # Check if the image was retrieved successfully
+    if r.status_code == 200:
+        r.raw.decode_content = True
+
+        # Open a local file with wb ( write binary ) permission.
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+
+    else:
+        print(f'{name} Couldn\'t be retreived')
+
+    date += dt
