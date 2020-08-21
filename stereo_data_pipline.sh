0d19ad511b6cd3e1f396252d4d828ee06153a2de
chemron <camerontasmith@gmail.com>
2020-08-21 16:30:27 +1000

    make stereo data pipeline

---
+++ stereo_data_pipline.sh
@@ -0,0 +1,13 @@
+python get_stereo_fits.py \
+        --path './DATA/fits_stereo' \
+        --start '2010-04-26 00:00:00' \
+        --end '2010-05-01 00:00:00' \
+        --wavelength 304
+
+        # --AIA \
+        # --HMI \
+        # --start '2011/01/01 00:00:00' \
+        # --end '2017/01/01 00:00:00' \
+        # --cadence 23 \
+        # --AIA_path './DATA/fits_aia' \
+        # --HMI_path './DATA/fits_hmi' \
