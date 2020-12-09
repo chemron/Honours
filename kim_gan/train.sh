075993c18f76bf0bf99e220017f0b988004c5540
chemron <camerontasmith@gmail.com>
2020-12-09 19:23:45 +1100

    more iterations

--- kim_gan/train.sh
+++ kim_gan/train.sh
@@ -1,11 +1,10 @@
 time python train.py \
-    --model_name "P100_cube_root" \
+    --model_name "P100_4" \
     --display_iter 50000 \
-    --max_iter 200000 \
-    --func "_cube_root"
+    --max_iter 300000 \
 
 # time python train.py \
-#     --model_name "mini_test_4" \
+#     --model_name "mini_test_5" \
 #     --display_iter 5 \
 #     --max_iter 20 \
-#     --func "_cube_root"
+
