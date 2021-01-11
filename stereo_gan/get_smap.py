a3caa4bfa728394a06544356119bcddf53bded0b
chemron <camerontasmith@gmail.com>
2021-01-11 16:02:54 +1100

    sort data

--- stereo_gan/get_smap.py
+++ stereo_gan/get_smap.py
@@ -2,9 +2,19 @@ import numpy as np
 import os
 
 dir_in = "../data_collection/DATA/np_phase_map/"
-dir_out = "DATA/S_MAP/"
+dir_out = "DATA/S_MAP"
 os.makedirs(dir_out) if not os.path.exists(dir_out) else None
 upsample = True
+
+
+def is_test(filename):
+    date = filename.split("_")[-2]
+    month = int(date.split(".")[1])
+    print(month)
+    if month >= 11:
+        return True
+    return False
+
 
 filenames = np.sort(os.listdir(dir_in))
 for filename in filenames:
@@ -17,5 +27,10 @@ for filename in filenames:
         img = cv2.resize(img,
                          dsize=(1024, 1024),
                          interpolation=cv2.INTER_CUBIC)
+
+    if is_test(filename):
+        dir_out += "_test/"
+    else:
+        dir_out += "_train/"
 
     np.save(dir_out + filename, img)
---
+++ stereo_gan/sort_data.py
@@ -0,0 +1,30 @@
+import numpy as np
+import os
+from shutil import copyfile
+
+dir_in = "../data_collection/DATA/np_phase_map/"
+dir_out_train = "DATA/S_MAP_train/"
+dir_out_test = "DATA/S_MAP_test/"
+for dir in (dir_out_train, dir_out_test):
+    os.makedirs(dir) if not os.path.exists(dir) else None
+
+upsample = True
+test_months = (11, 12)
+
+
+def is_test(filename):
+    date = filename.split("_")[-2]
+    month = int(date.split(".")[1])
+    return month in test_months
+
+
+filenames = np.sort(os.listdir(dir_in))
+for filename in filenames:
+    print(filename)
+    path_in = dir_in + filename
+    if is_test(filename):
+        path_out = dir_out_test + filename
+    else:
+        path_out = dir_out_train + filename
+
+    copyfile(path_in, path_out)
