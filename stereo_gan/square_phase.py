9fed746badc15bc951a3c4d0c82f95a9cf83f271
chemron <camerontasmith@gmail.com>
2021-01-11 14:15:57 +1100

    upsample seismic maps

--- stereo_gan/square_phase.py
+++ stereo_gan/square_phase.py
@@ -1,9 +1,10 @@
 import numpy as np
 import os
 
-dir_in = "DATA/np_phase_map/"
-dir_out = "DATA/np_phase_map_square/"
+dir_in = "../data_collection/DATA/np_phase_map/"
+dir_out = "DATA/S_MAP/"
 os.makedirs(dir_out) if not os.path.exists(dir_out) else None
+upsample = True
 
 filenames = np.sort(os.listdir(dir_in))
 for filename in filenames:
@@ -11,4 +12,10 @@ for filename in filenames:
     path = dir_in + filename
     img = np.load(path)
     img = img[:, 90:270]
+    if upsample:
+        import cv2
+        img = cv2.resize(img,
+                         dsize=(1024, 1024),
+                         interpolation=cv2.INTER_CUBIC)
+
     np.save(dir_out + filename, img)
--- stereo_gan/view_npy.py
+++ stereo_gan/view_npy.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 plt.switch_backend("agg")
-modes = ["MAG"]
+modes = ["S_MAP"]
 
 for mode in modes:
     folder = f"DATA/{mode}/"
 
-    filename = folder + os.listdir(folder)[0]
+    filename = folder + os.listdir(folder)[723]
     arr = np.load(filename)
     arr = np.nan_to_num(arr)
     plt.imsave(f"{mode}_test.png", arr, cmap="gray")
