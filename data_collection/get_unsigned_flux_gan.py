d54308fe5b8e068d3f52a06b7da31a8d523ecb47
chemron <camerontasmith@gmail.com>
2021-04-09 18:45:26 +1000

    plot flux

--- data_collection/get_unsigned_flux_gan.py
+++ data_collection/get_unsigned_flux_gan.py
@@ -5,62 +5,63 @@ from astropy.io import fits
 import os
 import glob
 
-folders = ["../DATA/TEST/", "../DATA/TRAIN/"]
+folders = ["../DATA/TRAIN/"]  # , "../DATA/TEST/"]
 save_dir = "DATA/unsigned_flux/"
 os.makedirs(save_dir) if not os.path.exists(save_dir) else None
 save_file = f"{save_dir}unsigned_flux_gan"
 modes = ["batch", "default", "low_tol", "ste"]
 mode_strs = ["p100_batch_1/100000*", "P100_default/200000*", "P100_low_tol/200000*", "MAG_*" ]
 
-for mode in modes:
-    save_str = f"{save_file}_{mode}.txt"
-    if os.path.exists(save_str):
-        print(f"File {save_str} already exists. Removing now.")
-        os.remove(save_str)
+# for mode in modes:
+#     save_str = f"{save_file}_{mode}.txt"
+#     if os.path.exists(save_str):
+#         print(f"File {save_str} already exists. Removing now.")
+#         os.remove(save_str)
 
 for folder in folders:
     for date in np.sort(os.listdir(folder)):
-        try:
-            hdul = fits.open(f"{folder}{date}/ste_header", memmap=False, ext=0)
-            hdul.verify("fix")
-            header = hdul[0].header
-            for i in range(len(modes)):
-                mode = modes[i]
-                mode_str = mode_strs[i]
-                data_file = glob.glob(f"{folder}{date}/{mode_str}")[0]
-                data = np.load(data_file)
+        hdul = fits.open(f"{folder}{date}/ste_header", memmap=False, ext=0)
+        hdul.verify("fix")
+        header = hdul[0].header
+        for i in range(len(modes)):
+            mode = modes[i]
+            mode_str = mode_strs[i]
+            data_string = f"{folder}{date}/{mode_str}"
+            print(data_string)
+            data_file = glob.glob(data_string)[0]
+            data = np.load(data_file)
 
-                # undo normalisation
-                data = np.abs(data)**2
-                data *= clip_max
+            # undo normalisation
+            data = np.abs(data)**2
+            data *= clip_max
 
-                shape = data.shape
+            shape = data.shape
 
-                # radius of sun in arcsec
-                r_sun_arc = header["RSUN"]
-                # radius of sun in pixels
-                r_sun_pix = [0.5 * shape[0], 0.5 * shape[1]]
-                
-                cdelt = [r_sun_arc/r_sun_pix[0], r_sun_arc/r_sun_pix[1]]
-                
-                # reference pixel
-                c_ref = [0.5 * shape[0], 0.5 * shape[1]]
+            # radius of sun in arcsec
+            r_sun_arc = header["RSUN"]
+            # radius of sun in pixels
+            r_sun_pix = [0.5 * shape[0], 0.5 * shape[1]]
+            
+            cdelt = [r_sun_arc/r_sun_pix[0], r_sun_arc/r_sun_pix[1]]
+            
+            # reference pixel
+            c_ref = [0.5 * shape[0], 0.5 * shape[1]]
 
-                area = get_pixel_areas(header, shape, cdelt, c_ref)
-                flux = area * np.absolute(data)
+            area = get_pixel_areas(header, shape, cdelt, c_ref)
+            flux = area * np.absolute(data)
 
-                # remove nans
-                flux = np.nan_to_num(flux)
+            # remove nans
+            flux = np.nan_to_num(flux)
 
-                total_flux = np.sum(flux)
+            total_flux = np.sum(flux)
 
-                date_str = header["DATE-OBS"]
+            date_str = header["DATE-OBS"]
 
-                f = open(f"{save_file}_{mode}.txt", "a+")
-                f.write(f"{date_str}\t{total_flux}\n")
-                f.close()
-        except Exception as e:
-            # This is obviously bad practice, but since I'm running this overnight I
-            # don't want it to halt for some error I didn't anticipate
-            print(f"Got error for date: {date}")
-            print(e)
+            f = open(f"{save_file}_{mode}.txt", "a+")
+            f.write(f"{date_str}\t{total_flux}\n")
+            f.close()
+        # except Exception as e:
+        #     # This is obviously bad practice, but since I'm running this overnight I
+        #     # don't want it to halt for some error I didn't anticipate
+        #     print(f"Got error for date: {date}")
+        #     print(e)
--- data_collection/plot_flux.py
+++ data_collection/plot_flux.py
@@ -4,40 +4,19 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 from matplotlib.dates import (MONTHLY, DAILY, DateFormatter,
                               rrulewrapper, RRuleLocator)
-# plt.switch_backend('agg')
-
-n = 5000
-
+plt.switch_backend('agg')
 
 def moving_average(a, n):
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
 
-dates = []
-fluxes = []
-data = open(sys.argv[1], "r")
-for line in data:
-    date, flux = line.split()
-    date = datetime.strptime(date[:-3], "%Y-%m-%dT%H:%M:%S")
-    flux = float(flux)
-    if flux >= 1e20:
-        continue
-    dates.append(date)
-    fluxes.append(flux)
-data.close
-
-x_flares = open("DATA/unsigned_flux/x_flare_list.txt", "r")
-x_dates = []
-for line in x_flares:
-    y, m, d = line.split("/")
-    y = int(y)
-    m = int(m)
-    d = int(d[:-1])
-    x_dates.append(datetime(year=y, month=m, day=d))
-data.close
+folder = "DATA/unsigned_flux/"
+modes = ["gan_batch", "gan_default", "gan_low_tol", "gan_ste"]
+n_m = len(modes)
 
 
+# set up figure
 plt.figure(1, figsize=(25, 10))
 ax = plt.subplot(1, 1, 1)
 # rule = rrulewrapper(MONTHLY, interval=6)
@@ -48,9 +27,70 @@ ax.xaxis.set_major_formatter(formatter)
 ax.xaxis.set_tick_params(rotation=30, labelsize=10)
 ax.set_xlabel("Date")
 ax.set_ylabel("Flux")
-for x_date in x_dates:
-    ax.axvline(x_date, c='r')
+# ax.set_ylim(1e18, 1e21)
+# colours
+cmap = list(plt.get_cmap("tab10").colors)
 
-plt.plot(dates, fluxes, c='k')
-# plt.show()
+# plot flares
+x_flares = open(f"{folder}x_flare_list.txt", "r")
+x_dates = []
+for line in x_flares:
+    y, m, d = line.split("/")
+    y = int(y)
+    m = int(m)
+    d = int(d[:-1])
+    x_dates.append(datetime(year=y, month=m, day=d))
+x_flares.close
+n_x = len(x_dates)
+
+
+for i in range(n_x):
+    x_date = x_dates[i]
+    ax.axvline(x_date, c='r', label="x class flare" if i==0 else "")
+
+# plot actual flux
+mode = 'hmi'
+data = open(f"{folder}unsigned_flux_{mode}.txt")
+dates = []
+fluxes = []
+for line in data:
+    date, flux = line.split()
+    # remove milliseconds
+    date = date.split(".")[0]
+    date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
+    flux = float(flux)
+    if flux >= 1e20:
+        continue
+    dates.append(date)
+    fluxes.append(flux)
+plt.plot(dates, fluxes, label=f"Flux according to {mode.upper()}", c='k')
+# clear variables
+dates = []
+fluxes = []
+data.close
+
+# plot for gans flux's
+dates = []
+fluxes = []
+for i in range(n_m):
+    mode = modes[i]
+    data = open(f"{folder}unsigned_flux_{mode}.txt")
+    for line in data:
+        date, flux = line.split()
+        # remove milliseconds
+        date = date.split(".")[0]
+        date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
+        flux = float(flux)
+        if flux >= 1e20:
+            continue
+        dates.append(date)
+        fluxes.append(flux)
+    plt.scatter(dates, fluxes, label=f"Flux according to {mode}", s=4)
+    # clear variables
+    dates = []
+    fluxes = []
+    data.close
+
+
+plt.legend()
 plt.savefig("Flux_vs_time.png")
